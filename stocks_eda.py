8865df3f456e27303ec2ea81e6516c8c55bacb3e
LoretaE <e.loreta@gmail.com>
2024-07-08 18:24:54 +0300

    Updates

--- stocks_eda.py
+++ stocks_eda.py
@@ -6,54 +6,69 @@ import seaborn as sns
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.width', 150)
 
-# Load the dataset
-stocks = pd.read_csv('Data/stocks_close.csv', index_col=[0], parse_dates=[0])
 
-print(stocks.head())
-print(stocks.info())
+def data_eda(failas):
+    # Load the dataset
+    stocks = pd.read_csv(failas, index_col=[0], parse_dates=[0])
+    # Normalising prices (to base value - 100%)
+    stocks_norm = stocks.div(stocks.iloc[0]).mul(100)
+    #  Stocks' yearly return (yearly value change)
+    yearly_return = stocks.resample('YE').last().pct_change(periods=1).mul(100)
+    print(yearly_return)
+    #  Yearly return (mean)
+    ann_return_mean = yearly_return.mean().round(2).sort_values(ascending=False)
+    print(ann_return_mean)
+    return stocks, stocks_norm, yearly_return, ann_return_mean
 
-#  Visualising stock prices
-plt.figure(figsize=(12, 6))
-stocks.plot()
-plt.title('Stock Prices')
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.legend()
-plt.show()
 
-# Visualising normalised stock prices
-stocks_norm = stocks.div(stocks.iloc[0]).mul(100)
-print(stocks_norm)
-plt.figure(figsize=(12, 6))
-stocks_norm.plot()
-plt.title('Stock Prices (normalised)')
-plt.xlabel('Date')
-plt.ylabel('Normalised Price (%)')
-plt.legend()
-plt.show()
+def visualisation(stocks, stocks_norm, yearly_return, ann_return_mean):
+    #  Visualising stock prices
+    plt.figure(figsize=(12, 6))
+    stocks.plot()
+    plt.title('Stock Prices')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.show()
 
-#  Stocks' return (yearly value change)
-yearly_return = stocks.resample('YE').last().pct_change(periods=1).mul(100)
-print(yearly_return)
+    # Visualising normalised stock prices
+    plt.figure(figsize=(12, 6))
+    stocks_norm.plot()
+    plt.title('Stock Prices (normalised)')
+    plt.xlabel('Date')
+    plt.ylabel('Normalised Price (%)')
+    plt.legend()
+    plt.show()
 
-plt.figure(figsize=(12, 6))
-yearly_return.plot()
-plt.title("Stocks' Return (yearly change, %)")
-plt.xlabel('Date')
-plt.ylabel('Stocks yearly return (%)')
-plt.xlim('2015', '2023')
-plt.legend()
-plt.show()
+    # Visualising yearly return
+    plt.figure(figsize=(12, 6))
+    yearly_return.plot()
+    plt.title("Stocks' Return (yearly change, %)")
+    plt.xlabel('Date')
+    plt.ylabel('Stocks yearly return (%)')
+    plt.xlim('2015', '2023')
+    plt.legend()
+    plt.show()
 
-sns.boxplot(data=yearly_return)
-plt.title('Stocks Return (yearly change, %) Boxplot')
-plt.show()
+    sns.boxplot(data=yearly_return)
+    plt.title('Stocks Return (yearly change, %) Boxplot')
+    plt.show()
 
-ann_return_mean = yearly_return.mean().round(2).sort_values(ascending=False)
-print(ann_return_mean)
+    # Visualising yearly return (mean)
+    sns.barplot(data=ann_return_mean)
+    plt.title('Stocks Annual Return (Mean, %)')
+    plt.xlabel('Stocks')
+    plt.ylabel('Annual Return (%)')
+    plt.show()
 
-sns.barplot(data=ann_return_mean)
-plt.title('Stocks Annual Return (Mean, %)')
-plt.xlabel('Stock')
-plt.ylabel('Annual Return (%)')
-plt.show()
+
+def main():
+    #  Loading data and calculating main indicators
+    stocks, stocks_norm, yearly_return, ann_return_mean = data_eda('Data/stocks_close.csv')
+
+    # Visualising EAD data
+    visualisation(stocks, stocks_norm, yearly_return, ann_return_mean)
+
+
+if __name__ == '__main__':
+    main()
